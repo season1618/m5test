926c2f4b0445620e7640d9442a04fe50ab1a551f
season1618 <seasonminority314@gmail.com>
2024-05-06 10:24:05 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -11,30 +11,11 @@ use esp_hal::{
     uart::{config::Config, TxRxPins, Uart},
 };
 
-extern crate alloc;
-use core::mem::MaybeUninit;
-
-#[global_allocator]
-static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
-
-fn init_heap() {
-    const HEAP_SIZE: usize = 32 * 1024;
-    static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
-
-    unsafe {
-        ALLOCATOR.init(HEAP.as_mut_ptr() as *mut u8, HEAP_SIZE);
-    }
-}
-
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take();
     let system = peripherals.SYSTEM.split();
-
     let clocks = ClockControl::max(system.clock_control).freeze();
-    init_heap();
-
-    esp_println::logger::init_logger_from_env();
 
     let mut timer = esp_hal::timer::TimerGroup::new(peripherals.TIMG0, &clocks, None).timer0;
 
@@ -48,6 +29,6 @@ fn main() -> ! {
     loop {
         timer.start(MicrosDurationU64::from_ticks(1000000));
         timer.wait();
-        uart1.write_bytes("\nHello, world!".as_bytes()).unwrap();
+        uart1.write_bytes("Hello, world!\n".as_bytes()).unwrap();
     }
 }
