f30398de4f736915bcfdeffbe6c14640ad8cb9ae
season1618 <seasonminority314@gmail.com>
2024-05-12 08:45:33 +0900

    ジャイロセンサ

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,9 @@ use esp32_hal::{
     peripherals::Peripherals,
     prelude::*,
     spi::{Spi, SpiMode},
+    timer::TimerGroup,
 };
+use esp_println::println;
 
 use display_interface_spi::SPIInterfaceNoCS;
 use embedded_graphics::{
@@ -39,7 +41,7 @@ fn main() -> ! {
     let cs = io.pins.gpio5;
     let dc = io.pins.gpio15.into_push_pull_output();
 
-    let i2c = I2C::new(
+    let mut i2c = I2C::new(
         peripherals.I2C0,
         io.pins.gpio21,
         io.pins.gpio22,
@@ -47,43 +49,46 @@ fn main() -> ! {
         &mut system.peripheral_clock_control,
         &clocks,
     );
-    let mut axp = Axp192::new(i2c);
-    m5sc2_init(&mut axp, &mut delay).unwrap();
 
-    let spi = Spi::new(
-        peripherals.SPI2,
-        sck,
-        mosi,
-        miso,
-        cs,
-        400u32.kHz(),
-        SpiMode::Mode0,
-        &mut system.peripheral_clock_control,
-        &mut clocks
-    );
-    let spi_iface = SPIInterfaceNoCS::new(spi, dc);
-    let mut lcd = Builder::ili9342c_rgb666(spi_iface)
-        .with_display_size(320, 240)
-        .with_color_order(ColorOrder::Bgr)
-        .with_invert_colors(ColorInversion::Inverted)
-        .init(&mut delay, None::<GpioPin<Output<PushPull>, 0>>) // tekitou
-        .unwrap();
+    const CONFIG: u8 = 0x01;
+    const PWR_MGMT_1: u8 = 0x6B;
+    const ACCEL: u8 = 0x3B;
+    const TEMP: u8 = 0x41;
+    const GYRO: u8 = 0x43;
+    const SLAVE_ADDR: u8 = 0x68;
 
-    // lcd.clear(Rgb666::RED).unwrap();
-
-    let style = MonoTextStyle::new(&FONT_6X10, Rgb666::BLACK);
-
-    Text::with_alignment(
-        "Hello, world!",
-        Point::new(20, 30),
-        style,
-        Alignment::Center,
-    )
-    .draw(&mut lcd)
-    .unwrap();
+    i2c.write(SLAVE_ADDR, &[PWR_MGMT_1, 0b1_0_0_0_0_000]).unwrap(); // reset
+    delay.delay_ms(1000u32);
+    i2c.write(SLAVE_ADDR, &[PWR_MGMT_1, 0b0_0_0_0_0_001]).unwrap(); // enable gyroscope
+    i2c.write(SLAVE_ADDR, &[CONFIG, 0b0_0_0_00_0_01]).unwrap();
 
     loop {
+        let mut accel_buf = [0; 6];
+        let mut gyro_buf = [0; 6];
+        let mut temp_buf = [0; 2];
+
+        i2c.write(SLAVE_ADDR, &[ACCEL]).unwrap();
+        i2c.read(SLAVE_ADDR, &mut accel_buf).unwrap();
+        i2c.write(SLAVE_ADDR, &[GYRO]).unwrap();
+        i2c.read(SLAVE_ADDR, &mut gyro_buf).unwrap();
+        i2c.write(SLAVE_ADDR, &[TEMP]).unwrap();
+        i2c.read(SLAVE_ADDR, &mut temp_buf).unwrap();
+
+        let accel = (concat(&accel_buf[0..2]) / 16384.0, concat(&accel_buf[2..4]) / 16384.0, concat(&accel_buf[4..6]) / 16384.0);
+        let gyro = (concat(&gyro_buf[0..2]) / 131.0, concat(&gyro_buf[2..4]) / 131.0, concat(&gyro_buf[4..6]) / 131.0);
+        let temp = concat(&temp_buf) / 326.8 + 25.0;
+
+        println!("accel (g m / s^2): {:5.2?}", accel);
+        println!("gyro (degree / s): {:5.2?}", gyro);
+        println!("temp  (degree C) : {temp}");
+        println!("");
+
+        delay.delay_ms(1000u32);
     }
+}
+
+fn concat(arr: &[u8]) -> f32 {
+    (((arr[0] as u16) << 8 | arr[1] as u16) as i16) as f32
 }
 
 fn m5sc2_init<I2C, E>(axp: &mut axp192::Axp192<I2C>, delay: &mut Delay) -> Result<(), E>
